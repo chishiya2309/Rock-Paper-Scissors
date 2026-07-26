05f3752263a47c67a5254a3b9282cb2a236f1c3c
chishiya2309 <lequanghung.work@gmail.com>
2026-07-26 09:58:36 +0700

    Write the logic to get the human choice

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 let choices = ["rock", "paper", "scissors"];
 
 let getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)];
+
+function getHumanChoice(choice) {
+    if (choice === 1) {
+        return "rock";
+    }else if(choice === 2) {
+        return "paper";
+    }else if(choice === 3) {
+        return "scissors";
+    }else {
+        return "invalid";
+    }
+}
