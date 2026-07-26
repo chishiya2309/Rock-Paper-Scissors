8106bd39d49751e690f7431cb64648f02c1e84d7
chishiya2309 <lequanghung.work@gmail.com>
2026-07-26 09:51:51 +0700

    refactor getComputerChoice function

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
 let choices = ["rock", "paper", "scissors"];
 
-function getComputerChoice() {
-    return choices[Math.floor(Math.random() * choices.length)];
-}
+let getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)];
