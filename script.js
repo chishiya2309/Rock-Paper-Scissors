385b5098c1f8f3a24d080e93c9946cb585cc2891
chishiya2309 <lequanghung.work@gmail.com>
2026-07-26 10:40:41 +0700

    Write the logic to play a single round

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 let choices = ["rock", "paper", "scissors"];
+let humanScore = 0;
+let computerScore = 0;
+
+const WINNING_RULES = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper"
+};
 
 let getComputerChoice = () => choices[Math.floor(Math.random() * choices.length)];
 
-function getHumanChoice(choice) {
+function getHumanChoice() {
+    let choice = parseInt(prompt("Enter your choice:\n1 - rock\n2 - paper\n3 - scissors"));
     if (choice === 1) {
         return "rock";
     }else if(choice === 2) {
         return "paper";
     }else if(choice === 3) {
         return "scissors";
-    }else {
-        return "invalid";
     }
-}
+};
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie! Both chose ${humanChoice}.`);
+        return;
+    }else if(WINNING_RULES[humanChoice] === computerChoice) {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+        humanChoice += 1;
+    }else {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+    }
+};
+
